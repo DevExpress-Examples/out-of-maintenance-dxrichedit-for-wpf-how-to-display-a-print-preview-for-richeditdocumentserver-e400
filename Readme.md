8cc031409b5045327537dd298127d754bacb585f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXRichEdit for WPF: How to display a print preview for RichEditDocumentServer
+
+
+<p>This example illustrates how to display a print preview of RichEditDocumentServer's document in the <a href="http://documentation.devexpress.com/#WPF/clsDevExpressXpfPrintingDocumentPreviewWindowtopic"><u>DocumentPreviewWindow</u></a> dialog from the <a href="http://www.devexpress.com/Products/NET/Controls/WPF/Printing/"><u>DXPrinting Library</u></a>.</p><p><strong>See Also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/E3842">DXRichEdit for WPF: How to specify a printer in code and print a document</a></p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraPrintingLinks
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.Services.Implementation
+Imports DevExpress.XtraRichEdit.Commands
+
+Namespace RichEditPreviewDocument
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim documentServer As New RichEditDocumentServer()
+			Dim link As New LegacyPrintableComponentLink(documentServer)
+			Dim model As LinkPreviewModel = New RichEditLinkPreviewModel(link)
+			Dim window As New DocumentPreviewWindow()
+
+			model.Link = link
+			documentServer.Text = "RichEditDocumentServer Print Preview."
+			link.CreateDocument(False)
+			window.Model = model
+			window.ShowDialog()
+		End Sub
+	End Class
+End Namespace
