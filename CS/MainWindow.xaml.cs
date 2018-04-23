c2a9da354d291d9de05c6cbd479887b384d79961
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Printing;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraPrintingLinks;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.Services.Implementation;
+using DevExpress.XtraRichEdit.Commands;
+
+namespace RichEditPreviewDocument {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            RichEditDocumentServer documentServer = new RichEditDocumentServer();
+            LegacyLinkPreviewModel model = new RichEditLinkPreviewModel();
+            PrintableComponentLinkBase link = new PrintableComponentLinkBase(new PrintingSystemBase());
+            DocumentPreviewWindow window = new DocumentPreviewWindow();
+
+            link.Component = documentServer;
+            model.Link = link;
+            documentServer.Text = "RichEditDocumentServer Print Preview.";
+            link.CreateDocument(false);
+            window.Model = model;
+            window.ShowDialog();
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraPrintingLinks
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.Services.Implementation
+Imports DevExpress.XtraRichEdit.Commands
+
+Namespace RichEditPreviewDocument
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim documentServer As New RichEditDocumentServer()
+			Dim model As LegacyLinkPreviewModel = New RichEditLinkPreviewModel()
+			Dim link As New PrintableComponentLinkBase(New PrintingSystemBase())
+			Dim window As New DocumentPreviewWindow()
+
+			link.Component = documentServer
+			model.Link = link
+			documentServer.Text = "RichEditDocumentServer Print Preview."
+			link.CreateDocument(False)
+			window.Model = model
+			window.ShowDialog()
+		End Sub
+	End Class
+End Namespace
