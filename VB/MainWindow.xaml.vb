baf49c168c90bfe926bef69af74cc06b3d0a6d34
DevExpressExampleBot <fakeEmail>
2021-09-02 18:52:30 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -7,21 +7,21 @@ Imports DevExpress.XtraRichEdit.Services.Implementation
 Imports DevExpress.XtraRichEdit.Commands
 
 Namespace RichEditPreviewDocument
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim documentServer As New RichEditDocumentServer()
-            documentServer.Text = "RichEditDocumentServer Print Preview."
-            Dim link As New LegacyPrintableComponentLink(documentServer)
-            Dim window As New DocumentPreviewWindow()
-            window.PreviewControl.DocumentSource = link
-            link.CreateDocument(False)
-            window.ShowDialog()
-        End Sub
-    End Class
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim documentServer As New RichEditDocumentServer()
+			documentServer.Text = "RichEditDocumentServer Print Preview."
+			Dim link As New LegacyPrintableComponentLink(documentServer)
+			Dim window As New DocumentPreviewWindow()
+			window.PreviewControl.DocumentSource = link
+			link.CreateDocument(False)
+			window.ShowDialog()
+		End Sub
+	End Class
 End Namespace
